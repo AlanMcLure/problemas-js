4948f40dda7821ea01b4347daaeef25c46713078
AlanMcLure <alanmclurealarcon02@gmail.com>
2023-11-30 00:21:31 +0100

    explicaciones

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ for (let i = 1; i <= 10; i++) {
     }, (j - 1) * 1000 + i * 100);
   }
 }
+// 1 1
+// 1 2
+// 1 3
+// ...
+// 10 1
+// 10 2
+// 10 3
+// ...
+// 10 10
+
+// Todos los console.log se ejecutan después del período de tiempo especificado en: '(j - 1) * 1000 + i * 100'
